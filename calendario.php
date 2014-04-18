6de72897846c614770b8e97b889ed3825b327070
Felicce <exosymmetry@hotmail.com>
2014-04-17 21:23:23 -0430

    Calendarip Update

--- calendario.php
+++ calendario.php
@@ -1,12 +1,9 @@
 <?php include 'calendario/includes/embed_setup.php'; ?>
 <html>
   <head>
-    <title>Example Embedded Page</title>
+    <title>GlobalSys</title>
   </head>
   <body>
-    <h1>Example Page Header</h1>
     <?php include 'calendario/includes/embed.php'; ?>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    	<script type="text/javascript">var jQuery_1_9_2 = $.noConflict(true);</script>
   </body>
 </html>
