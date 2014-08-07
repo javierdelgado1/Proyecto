471799ed4a5df565c748104c2bf91829d9c20b30
cheche338 <cheche338@gmail.com>
2014-08-06 22:21:06 -0430

    Agregando funcionalidad a los select de la vista principal

--- js/eventosnuevo.js
+++ js/eventosnuevo.js
@@ -1,4 +1,41 @@
 function eventos(){	
+	categoria1.onchange=function(){
+		var area = categoria1.options[categoria1.selectedIndex].value;
+		var result;
+		switch(area){
+			case "Perforacion":
+				result = ConsultaCursos(1);
+				break;
+			case "Yacimiento":
+				result = ConsultaCursos(2);
+				break;
+			case "Administracion":
+				result = ConsultaCursos(3);
+				break;
+			case "Finanzas":
+				result = ConsultaCursos(4);
+				break;
+			default:
+				curso1.innerHTML = "";
+				var option = document.createElement("option");
+				option.text = "Selección por curso";
+				option.value = "";
+				curso1.add(option);
+				return;
+				break;
+		}
+		LlenarSelectCursos(result);
+	}
+	curso1.onchange=function(){
+		var cursoID = curso1.options[curso1.selectedIndex].value;
+		if(cursoID == "")
+			return;
+		VaciarFileInput();
+		fileupload.action = "server/php/PortafolioVerAdjuntos.php?id="+cursoID;
+		mainInputFile();
+		$("#ModalDescargarArchivo").modal("show");
+	}
+
 	laempresa.onclick=function(){			 
 		 $.ajax({  
             url: 'formas/laempresa.html',  
@@ -36,3 +73,53 @@ function eventos(){
 	}	
 
 }
+	function LlenarSelectCursos(datosCurso){
+		curso1.innerHTML = "";
+		var option = document.createElement("option");
+		option.text = "Selección por curso";
+		option.value = "";
+		curso1.add(option);
+		for(i=0; i < datosCurso[0].m; i++){
+			var option = document.createElement("option");
+			option.text = datosCurso[i].Curso;
+			option.value = datosCurso[i].id;
+			curso1.add(option);
+		}
+    }
+	function ConsultaCursos(tipo){
+		var result;
+		$.ajax
+		({
+			type: "POST",
+			url: "Modelo/consultasPortafolio.php",
+			data: {id:2, tipo:tipo},
+			async: false,
+			dataType: "json",
+			success:
+			function (msg)
+			{
+				result = msg;
+			},
+		error:
+		function (msg) {
+			console.log("No se pudo realizar la conexion");}
+		});
+		return result;
+	}
+		function VaciarFileInput(){
+		// Eliminando visualmente los archivos cargados
+		var trs = $('tr.template-download');
+		if (typeof trs != "undefined") {
+			for(var i = trs.length - 1; i > -1; i--)
+			{
+			    trs[i].outerHTML = "";
+			}
+		}
+		trs = $('tr.template-upload');
+		if (typeof trs != "undefined") {
+			for(var i = trs.length - 1; i > -1; i--)
+			{
+			    trs[i].outerHTML = "";
+			}
+		}
+	}
